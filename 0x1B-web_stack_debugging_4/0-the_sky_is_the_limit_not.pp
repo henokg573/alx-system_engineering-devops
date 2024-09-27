ecb2f7576eea96166a0e81b86135de1ac39f6813
Henok Girma <140075019+henokg573@users.noreply.github.com>
2024-09-27 16:52:58 +0300

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,10 +3,3 @@ exec { 'fix--for-nginx':
   command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
   path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
-# Restart nginx
-exec { 'nginx start':
-    # Restart the nginx service
-    command => '/etc/init.d/nginx restart',
-    # specify the path for the init.d script
-    path => '/etc/init.d',
-}
